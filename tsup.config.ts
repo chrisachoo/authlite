426ce5c5e45ab8aab5cbe133f47d4f3204b33d72
chrisachoo <mabs.chris@outlook.com>
2025-10-20 23:01:23 +0200

    fix: reorder entry points in tsup config and streamline type export in types.entry.ts

--- tsup.config.ts
+++ tsup.config.ts
@@ -4,9 +4,9 @@ export default defineConfig({
 	clean: true,
 	dts: true,
 	entry: {
-		index: "src/index.ts",
-		types: "src/types.entry.ts",
-		"drizzle-sqlite": "src/adapters/drizzle-sqlite.ts"
+		"drizzle-sqlite": "src/adapters/drizzle-sqlite.ts",
+		"index": "src/index.ts",
+		"types": "src/types.entry.ts"
 	},
 	external: ["better-sqlite3", "drizzle-orm", "ioredis", "@node-rs/argon2"],
 	format: ["esm", "cjs"],
